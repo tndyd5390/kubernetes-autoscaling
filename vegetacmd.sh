a523d16c11016f50cb3573d5a7ea1546cd36b4bd
waterdragon <tndyd5390@naver.com>
2022-01-04 00:32:15 +0900

    txt to sh

--- vegetacmd.sh
+++ vegetacmd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "GET http://127.0.0.1:30080" | vegeta attack -rate=30 -duration=8m | vegeta report
+echo "GET http://127.0.0.1:30080" | vegeta attack -rate=1 -duration=8m | vegeta report
